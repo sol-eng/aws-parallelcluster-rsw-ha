344463705d6cf95324ac51cb9b0eb5a0fc4a30bc
Michael Mayer <michael.mayer@posit.co>
2023-11-30 20:31:39 +0100

    fix missing if

--- image/install-pwb.sh
+++ image/install-pwb.sh
@@ -82,4 +82,4 @@ if [ `rstudio-server version | cut -d "+" -f 1 | sed 's/\.//g'` -lt 2023120 ]; t
     curl -O https://cdn.rstudio.com/launcher/releases/bionic/launcher-bionic-amd64-2.15.1-5.tar.gz && \
     tar xvfz launcher-* -C /usr/lib/rstudio-server/bin  --strip-components=1 && \
     rm -f launcher-* && popd 
-
+fi
