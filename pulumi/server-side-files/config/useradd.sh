39eef60434de615cc29efca9dfb278e2f50810ab
Michael Mayer <michael.mayer@posit.co>
2023-12-16 14:03:11 +0100

    update useradd.sh with better error correction

--- pulumi/server-side-files/config/useradd.sh
+++ pulumi/server-side-files/config/useradd.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-N=20
+N=40
 
 for i in \`seq 1 \$1\`
 do
-    (	
+    (
+	username=posit\`printf %04i \$i\`
+    	if ( ! id \$username >& /dev/null ); then 	
         while true
-            do
-                username=posit\`printf %04i \$i\`
-                if ( ! id \$username >& /dev/null ); then
-                    echo "creating user \$username"
-	                expect create-users.exp \$username {{user_password}} >& /dev/null
-                    #echo {{user_password}} | pamtester login \$username authenticate
-                else
-                    echo "user \$username already exists"
-                fi
-                if [ \$? -eq 0 ]; then
+            do  
+		sleep \`echo 2*\$(( \$i % \$N ))/\$N | bc -l\`
+                echo creating user \$username
+		if ( ! id \$username >& /dev/null ); then 
+			expect create-users.exp \$username {{user_password}} >& /dev/null 
+		fi
+		sleep 5 
+		if ( echo {{user_password}} | pamtester login \$username authenticate ); then  
                     break 
                 fi
             done
+	fi
     ) &
     if [[ \$(jobs -r -p | wc -l) -ge \$N ]]; then
         wait -n
