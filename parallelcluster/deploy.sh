454dc38d0b0d8fc5b2dda36e06d82ed79980f365
Michael Mayer <michael.mayer@posit.co>
2023-12-16 13:55:27 +0100

    aligning deploy.sh with changes in pulumi recipe

--- parallelcluster/deploy.sh
+++ parallelcluster/deploy.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-CLUSTERNAME="full"
-S3_BUCKETNAME="hpc-scripts1234a"
+CLUSTERNAME="usual"
+S3_BUCKETNAME="hpc-scripts1234-bm-old"
 SECURITYGROUP_RSW="sg-09ca531e5331195f1"
-AMI="ami-087ccbe156d606047"
+AMI="ami-01f00a49c53bfbb46" # final / bleeding edge wb 
+AMI="ami-075a426f0133659ff" # old / 2023.09.1
 REGION="eu-west-1"
-SINGULARITY_SUPPORT=true
-CONFIG="default"
+SINGULARITY_SUPPORT=false
+CONFIG="benchmark"
 
 echo "Extracting values from pulumi setup"
 SUBNETID=`cd ../pulumi && pulumi stack output vpc_subnet` 
 KEY=`cd ../pulumi && pulumi stack output "key_pair id" `
-DOMAINPWSecret=` cd ../pulumi && pulumi stack output "DomainPWARN" `
-CERT="../pulumi/key.pem"
-EMAIL=`cd ../pulumi && pulumi stack output 'key_pair id' | cut -d "-" -f 1`
+DOMAINPWSecret=` cd ../pulumi && pulumi stack output "domain_password_arn" `
+CERT="${KEY}.pem"
+EMAIL=`echo $KEY | cut -d "-" -f 1`
 AD_DNS=`cd ../pulumi && pulumi stack output ad_dns_1`
 DB_HOST=`cd ../pulumi && pulumi stack output db_address`
 DB_USER=`cd ../pulumi && pulumi stack output db_user`
 DB_PASS=`cd ../pulumi && pulumi stack output db_pass`
+SECURE_COOKIE_KEY=`cd ../pulumi && pulumi stack output secure_cookie_key`
+
 
 echo "preparing scripts" 
 rm -rf tmp
@@ -29,7 +32,9 @@ cat scripts/install-pwb-config.sh | \
         sed "s#DB_HOST#${DB_HOST}#g" | \
         sed "s#DB_USER#${DB_USER}#g" | \
        	sed "s#DB_PASS#${DB_PASS}#g" | \
+        sed "s#SECURE_COOKIE_KEY#${SECURE_COOKIE_KEY}#g" | \
         sed "s#SINGULARITY_SUPPORT#${SINGULARITY_SUPPORT}#g" \
+	sed "s#CLUSTER_CONFIG#${CONFIG}#g" \
 	> tmp/install-pwb-config.sh 
 
 aws s3 cp tmp/ s3://${S3_BUCKETNAME} --recursive 
