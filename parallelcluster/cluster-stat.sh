2b342a09023ec05b76d1e6f2ac6c1ec376b8bdbc
Michael Mayer <michael.mayer@posit.co>
2024-01-11 13:56:59 +0100

    improvements to cluster-stat.sh to show only running instances

--- parallelcluster/cluster-stat.sh
+++ parallelcluster/cluster-stat.sh
@@ -5,7 +5,7 @@ export clustername=$1
 echo "Information for cluster $clustername"
 echo "------------------------------------------"
 echo ""
-headnode=$(aws ec2 describe-instances --filters "Name=tag:parallelcluster:cluster-name,Values=$clustername" "Name=tag:parallelcluster:node-type,Values='HeadNode'" | jq -r '.Reservations |  .[] | .Instances | .[] | .InstanceId')
+headnode=$(aws ec2 describe-instances --filters "Name=tag:parallelcluster:cluster-name,Values=$clustername" "Name=tag:parallelcluster:node-type,Values='HeadNode'" --query 'Reservations[*].Instances[?State.Name==`running`]' | jq -r '.[] | .[] | .InstanceId')
 
 cmdid=$(aws ssm send-command  --instance-ids $headnode --document-name "AWS-RunShellScript" --parameters commands="rstudio-server version" --query "Command.CommandId" --output text )
 echo "Posit Workbench Version: `aws ssm list-command-invocations --command-id $cmdid --query 'CommandInvocations[].CommandPlugins[].{Status:Status,Output:Output}' --details | jq -r '.[] | .Output'`"
@@ -15,5 +15,5 @@ echo ""
 echo "Posit User Password: `export cwd=$CWD && cd ../pulumi && pulumi stack select $clustername && pulumi stack output user_password && cd $cwd `"
 echo ""
 echo "#InstanceID, PrivateDnsName, PublicDnsName, Role"
-aws ec2 describe-instances --filters "Name=tag:parallelcluster:cluster-name,Values=$clustername" | \
-    jq -r '.Reservations |  .[] | .Instances | .[] | [.InstanceId,.PrivateDnsName,.PublicDnsName,(.Tags | .[] | (select(.Key=="parallelcluster:node-type")) | .Value) ] | @csv'
+aws ec2 describe-instances --filters "Name=tag:parallelcluster:cluster-name,Values=$clustername" --query 'Reservations[*].Instances[?State.Name==`running`]' | jq -r '.[] | .[] | [.InstanceId,.PrivateDnsName,.PublicDnsName,(.Tags | .[] | (select(.Key=="parallelcluster:node-type")) | .Value)] | @csv'
+
