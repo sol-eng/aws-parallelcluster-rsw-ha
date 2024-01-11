3099a657ef7b0e05cd3bd9a7c531080630697fc3
Michael Mayer <michael.mayer@posit.co>
2024-01-11 13:58:28 +0100

    step back for pulumi to allow ssh again

--- pulumi/server-side-files/config/useradd.sh
+++ pulumi/server-side-files/config/useradd.sh
@@ -2,26 +2,26 @@
 
 N=40
 
-for i in `seq 1 $1`
+for i in \`seq 1 \$1\`
 do
     (
-	username=posit`printf %04i $i`
-    	if ( ! id $username >& /dev/null ); then 	
+	username=posit\`printf %04i \$i\`
+    	if ( ! id \$username >& /dev/null ); then 	
         while true
             do  
-		sleep `echo 2*$(( $i % $N ))/$N | bc -l`
-                echo creating user $username
-		if ( ! id $username >& /dev/null ); then 
-			expect create-users.exp $username {{user_password}} >& /dev/null 
+		sleep \`echo 2*\$(( \$i % \$N ))/\$N | bc -l\`
+                echo creating user \$username
+		if ( ! id \$username >& /dev/null ); then 
+			expect create-users.exp \$username {{user_password}} >& /dev/null 
 		fi
 		sleep 5 
-		if ( echo {{user_password}} | pamtester login $username authenticate ); then  
+		if ( echo {{user_password}} | pamtester login \$username authenticate ); then  
                     break 
                 fi
             done
 	fi
     ) &
-    if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+    if [[ \$(jobs -r -p | wc -l) -ge \$N ]]; then
         wait -n
     fi
 done
