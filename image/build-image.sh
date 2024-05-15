078a2dbbf57924d5d62113eaa0e86208b5d06699
Michael Mayer <michael.mayer@posit.co>
2024-05-15 11:34:44 +0200

    fix naming of image

--- image/build-image.sh
+++ image/build-image.sh
@@ -3,5 +3,5 @@ do
 aws s3 cp $i s3://hpc-scripts1234/image/$i
 done
 
-pcluster build-image -c image-config.yaml -i img-2024_04_1_748_pro2 
+pcluster build-image -c image-config.yaml -i img-2024-04-1-748-pro2 
 
