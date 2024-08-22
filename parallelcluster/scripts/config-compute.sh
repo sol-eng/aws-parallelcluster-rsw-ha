108d44e8c6b0476082ac32c6ee1137a9070b2afc
Michael Mayer <michael.mayer@posit.co>
2024-08-22 11:14:01 +0200

    update config-compute.sh to allow for new CUDA drivers

--- parallelcluster/scripts/config-compute.sh
+++ parallelcluster/scripts/config-compute.sh
@@ -25,3 +25,29 @@ if ( ! grep posit0001 /etc/sudoers >& /dev/null ); then
         echo "posit0001   ALL=NOPASSWD: ALL" >> /etc/sudoers
 fi
 
+#setup GPUs
+
+if ( lspci | grep NVIDIA ); then 
+   wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+   mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+   apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+   add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+   apt-get update
+   apt-get -y install cuda libcudnn8-dev
+   rmmod gdrdrv
+   rmmod nvidia
+   modprobe nvidia
+   apt clean
+   apt install -y nvidia-dkms-560 nvidia-kernel-source-560
+fi
+
+echo "posit0001   ALL=NOPASSWD: ALL" >> /etc/sudoers
+
+if (EASYBUILD_SUPPORT); then 
+    apt-get update && apt-get install -y lmod 
+    cat << EOF > /etc/profile.d/modulepath.sh
+#!/bin/bash
+
+export MODULEPATH=/opt/apps/easybuild/modules/all
+EOF
+fi  
