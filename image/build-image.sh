b769e305dc35f19a5e60639fa8d48a04d397c5c0
Michael Mayer <michael.mayer@posit.co>
2024-11-18 11:53:27 +0100

    use default S3 bucket

--- image/build-image.sh
+++ image/build-image.sh
@@ -1,6 +1,6 @@
 for i in install*.sh *.R
 do
-aws s3 cp $i s3://hpc-scripts-ide-team-556a5ad/image/$i
+aws s3 cp $i s3://hpc-scripts1234/image/$i
 done
 
 pcluster build-image -c image-config.yaml -i $1
